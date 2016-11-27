ca49721904a93c996f314954589386f1413e8500
Daniel Asher <dan@lexilabs.co>
2016-11-27 08:10:19 +0000

    Add support for Carthage

---
+++ RxGesture/RxGesture/RxGesture.h
@@ -0,0 +1,19 @@
+//
+//  RxGesture.h
+//  RxGesture
+//
+//  Created by Daniel Asher on 27/11/2016.
+//  Copyright © 2016 LEXI LABS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxGesture.
+FOUNDATION_EXPORT double RxGestureVersionNumber;
+
+//! Project version string for RxGesture.
+FOUNDATION_EXPORT const unsigned char RxGestureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxGesture/PublicHeader.h>
+
+
